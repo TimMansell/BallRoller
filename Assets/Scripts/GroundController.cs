c4f6e8d5a0282f65f3a0d85b3be07fc638862ca3
TimMansell <mansell.tim@gmail.com>
2018-09-06 20:29:18 +1000

    Add project to repo

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+    private void FixedUpdate()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.right * moveVertical);
+        transform.Rotate(Vector3.forward * moveHorizontal);
+    }
+}
